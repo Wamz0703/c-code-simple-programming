ed910aad97a62b21646e6844bb2e44968d648f2d
Wamz Mundia <wamuyuwamundia@gmail.com>
2023-10-22 01:10:11 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+  double x;
+  double y;
+  cout << "Enter x and y\n";
+  cin >> x >> y;
+  double sum = x + y;
+  cout << "sum: "<< sum <<endl;
+
+    
+ return 0;
+}
+
+    
+
