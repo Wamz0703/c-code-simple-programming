88600895c7ea2be54770b7332632f4f0dc9a935c
Wamz Mundia <wamuyuwamundia@gmail.com>
2023-11-19 22:58:25 +0300

    Add files via upload

--- code2.cpp
+++ code2.cpp
@@ -1,18 +1,28 @@
-#include <iostream>
-int fibonacci(int n){
-    if(n == 0)
-    return 0;
-    else if(n ==1)
-    
-    return 1;
-    else
-    return(fibonacci(n - 1) + fibonacci(n - 2) );
+#include<iostream>
+
+bool isPrime(int num){
+    if(num <=1){
+        return false;
+    }
+    for(int i = 2; i*i<= num; i++){
+        if(num%i==0){
+            return false;
+        }
+    }
+    return true;
 }
+ 
 int main(){
-    int n;
-    std::cout <<"Enter the value of n: ";
-    std::cin >> n;
-    std::cout << "The " << n << "th fibonacci number is: " << fibonacci(n - 1)<<"\n";
-   
+    int count = 0;
+    int num = 2;
+    std::cout<<"first 100 prime numbers are:"<< std::endl;
+    while(count < 100){
+        if (isPrime(num)){
+            std::cout<<num<<" "<<"\n";
+            count++;
+            
+        }
+        num++;
+    }
     return 0;
 }
